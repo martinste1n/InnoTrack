592acfd43b1bb14d5d7be0c6ce3f64a200f25835
Martin Stein <martin.stein@uni-siegen.de>
2016-01-13 16:01:31 +0100

    Initial Project Commit

---
+++ app/src/main/java/wineme/de/innotrack/ServiceStatus.java
@@ -0,0 +1,13 @@
+package wineme.de.innotrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_status);
+    }
+}
